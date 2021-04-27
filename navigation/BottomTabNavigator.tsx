d1817bd7aabdf38b50b1590e5efae3cd225b7f7b
bohdan-nitro <bogdantorgonsky@gmail.com>
2021-04-28 01:16:02 +0300

    HomeScreen

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -6,7 +6,7 @@ import * as React from 'react';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
-import HomeScreen from "../screens/TabOneScreen"
+import HomeScreen from "../screens/HomeScreen"
 import TabTwoScreen from '../screens/TabTwoScreen';
 import { BottomTabParamList, HomeParamList, TabTwoParamList } from '../types';
 
---
+++ screens/HomeScreen/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import styles from "./style"
+import { View} from '../../components/Themed';
+
+
+import categories from "../../assets/data/categories";
+import HomeCategory from "../../components/HomeCategory";
+import {FlatList} from "react-native";
+
+
+const HomeScreen = () => {
+    return (
+        <View style={styles.container}>
+            <FlatList data={categories.items} renderItem={({item}) => <HomeCategory category={item}/>}/>
+        </View>
+    );
+}
+export default HomeScreen;
